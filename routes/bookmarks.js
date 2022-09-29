e147d53f6bf02aa1fa12a4b1e5bba7f0761eccd7
Jaeyoon Song <jaeyoons@mit.edu>
2022-09-28 23:28:32 -0400

    Update bookmarks.js

--- routes/bookmarks.js
+++ routes/bookmarks.js
@@ -23,35 +23,37 @@ const router = express.Router();
  * @param {string} name - name of Bookmark (link will be /:Bookmark)
  * @param {string} url - link Bookmark points to
  * @return {Bookmark} - the created Bookmark
- * @throws {TODO1-2: Response Status} - if name is already taken
+ * @throws {TODO3: Response Status} - if name is already taken
  */
-router.post('TODO1-1: RESTful API endpoint', (req, res) => {
+// TODO2: Change the "get" to a proper method for creating a bookmark
+router.get('TODO1: RESTful API endpoint (URL)', (req, res) => {
   if (Bookmarks.findOne(req.body.name) !== undefined) {
-    res.status(/* TODO1-2: Response Status */).json({
+    res.status(/* TODO3: Response Status */).json({
       error: `Bookmark ${req.body.name} already exists.`,
     }).end();
   } else {
     const Bookmark = Bookmarks.addOne(req.body.name, req.body.url); 
-    res.status(/* TODO1-3: Response Status */).json(Bookmark).end();
+    res.status(/* TODO4: Response Status */).json(Bookmark).end();
   }
 });
 
 /**
  * Delete a Bookmark.
  * 
- * @name DELETE TODO2-1: RESTful API endpoint
+ * @name DELETE TODO5: RESTful API endpoint
  * 
  * @return {Bookmark} - the deleted Bookmark
- * @throws {TODO2-2: Response Status} - if Bookmark does not exist
+ * @throws {404} - if Bookmark does not exist
  */
-router.delete('TODO2-2: Response Status', (req, res) => {
+// TODO6: Change the "get" to a proper method for deleting a bookmark
+router.get('TODO5: RESTful API endpoint (URL)', (req, res) => {
   if (Bookmarks.findOne(req.params.name) === undefined) {
-    res.status(/* TODO2-2: Response Status */).json({
+    res.status(404).json({
       error: `Bookmark URL ${req.params.name} does not exist.`,
     }).end();
   } else {
     const Bookmark = Bookmarks.deleteOne(req.params.name);
-    res.status(/* TODO2-3: Response Status */).json(Bookmark).end();
+    res.status(200).json(Bookmark).end();
   }
 });
 
